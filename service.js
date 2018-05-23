5b31cc0310b1a1f0d62f67b95eb7633b092090ed
xiajunyi <309292581@qq.com>
2018-05-23 16:48:13 +0800

    webhook测试

--- service.js
+++ service.js
@@ -1,18 +1,15 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-
-    //  HTTP ͷ    // HTTP ״ֵ̬: 200 : OK
-    // : text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
-
-
-    //  "Hello World"
     response.end('Hi! xiajunyi\'s nodeJs Sever is Ok!\n');
 	//开始准备调用脚本
 	var callfile = require('child_process');
 	callfile.execFile('/opt/gitCode/hexoHook.sh',null,null,function (err, stdout, stderr) {
-     callback(err, stdout, stderr);
+     if (stderr) {
+       console.log(stderr);
+     }
+     console.log(stdout);
  });
 }).listen(8888);
 
